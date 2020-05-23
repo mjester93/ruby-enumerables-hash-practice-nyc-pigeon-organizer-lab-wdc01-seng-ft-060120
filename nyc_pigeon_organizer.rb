c97be277ac54f467e0d38abe3dde043a63600cb5
Michael Jester <m.jester.93@gmail.com>
2020-05-23 04:53:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,14 @@ def nyc_pigeon_organizer(data)
 
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-      pigeons[names][key] = inner_key
+      names.each do |names|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
     end
   end
 
