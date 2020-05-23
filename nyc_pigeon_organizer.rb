c84e4d32ce2268fb6548565731c290e0ada631e1
Michael Jester <m.jester.93@gmail.com>
2020-05-23 05:09:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value), hash|
+    value.each do |(inner_key, inner_values)|
+      inner_values.each do |name|
+        if !hash[name]
+          hash[name] = {}
+        end
+
+        if !hash[name][key]
+          !hash[name][key] = []
+        end
+        hash[name][key].push(inner_key.to_s)
+      end
+    end
+  end
 end
